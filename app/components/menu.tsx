1fceaba6324cf82c683d5c605ffb283d2d81ac6f
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2023-06-25 19:48:04 -0700

    key issue solved?

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -14,14 +14,15 @@ const MenuDisplay = () => {
                         <h1 className='text-2xl'>{item.header}</h1>
                         {
                             item.items.map((detail:({name:(string),description:(string),smPrice:(string),mdPrice:(string),lgPrice:(string)}), index:(number)) => (
+                            <div key={index}>
                             <MenuItem
-                            key={index}
                             name={detail.name}
                             description={detail.description}
                             smPrice={detail.smPrice}
                             mdPrice={detail.mdPrice}
                             lgPrice={detail.lgPrice}
                             />
+                            </div>
                         ))  
                         }
                     </div>
